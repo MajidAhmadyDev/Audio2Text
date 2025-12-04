b9a3640106496eeda637a51a346922d5f2309228
Majid Ahmady <myniceprojects@gmail.com>
2025-12-04 19:47:10 +0100

    add function to calculate memory size and time to load dataset and make dataloader

--- Sound2Text.py
+++ Sound2Text.py
@@ -42,7 +42,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 root = 'M:/Git/Sound2Text/Dataset-LJSpeech'
 seed = 42
-
+batch_size = 64
 
 # Load Dataset
 df = pd.read_csv(f'{root}/metadata.csv', 
@@ -156,7 +156,7 @@ vocab = build_vocab_from_iterator(
 vocab.set_default_index(1)
 print(vocab.get_itos()) # ['=', '<', ' ', 'e', 't', 'a', ...
 len(sorted(vocab.get_itos())) # 43
-
+vocab(['<', 'a', 'c', '>'])   # [2, 7, 16, 3]
 torch.save(vocab, 'vocab.pt')
 
 ################################################################################
@@ -190,35 +190,106 @@ print(1e3*(time.time()-s)/n)    # on GPU: 6.57ms / on CPU: 31.1
 print(mel_specgram.shape) # [100, 1, 128, 465]
 ################################################################################
 
+# Calculate the time spent loading an audio file
+start = time.time()
+
+for path in df_train['path']:
+  waveform, sample_rate = torchaudio.load(path)
+
+end = time.time()
+
+print(f'{end-start:.2f}s', f"{1e3*(end-start)/len(df_train['path']):.2f} ms")
+
+# Calculate the total RAM occupation by dataset
+total_nbytes = 0
+
+for path in df_train['path']:
+  waveform, _ = torchaudio.load(path)
+  total_nbytes += waveform.nbytes
+
+print(f'{total_nbytes/1e6} GB')
+################################# From Here to run #####################################
+
+# Custom dataset
+class LJSpeechDataset(Dataset):
+  def __init__(self, root: str, csv_file: str,
+               input_transform: Optional[Callable] = None, # .MelSpectrogram(Voices)
+               target_transform: Optional[Callable] = None,# vocab(text)
+               memory: Optional[bool] = False):
+
+    self.data = pd.read_csv(os.path.join(root, csv_file))
+    self.phase = csv_file.split('-')[0].capitalize()
+    self.input_transform = input_transform # .MelSpectrogram shift in Model
+    self.target_transform = target_transform 
+    self.memory = memory
+
+    self.sos = target_transform(['<']) # 2
+    self.eos = target_transform(['>']) # 3
+
+    if memory:
+      self._save_memory()
+
+  def __len__(self):
+    return len(self.data)
+
+  def __getitem__(self, idx):
+    if self.memory:
+      waveform = self.audios[idx].clone().squeeze()
+    else:
+      waveform = torchaudio.load(self.data.iloc[idx, 3])[0].squeeze()
+
+    transcript = self.data.iloc[idx, 1]
+
+    #self.target_transform is equall vocab(['a', 'c']) -> [7, 16]
+    transcript = self.target_transform(list(transcript.lower()))
+    transcript = self.sos + transcript + self.eos
+    transcript = torch.LongTensor(transcript)
+
+    return waveform, transcript
+
+  def _save_memory(self):
+    self.audios = []
+    for path in self.data['path']:
+      self.audios.append(self._load_audio(path))
+
+  def _load_audio(self, path):
+    return torchaudio.load(path)[0] # 0: waveform, 1: sample_rate
+
+  def __repr__(self): # if print(train_set) will be displayed this
+    return f"""Number of Datapoints: {len(self.data)}\nPhase: {self.phase}"""
 
 
+vocab = torch.load('vocab.pt')
+train_set = LJSpeechDataset(root=root, csv_file='train-subset.csv',
+                            target_transform=vocab, memory=True)
+print(train_set) # Number of Datapoints: 9825  ,  Phase: Train
 
+valid_set = LJSpeechDataset(root=root, csv_file='valid-subset.csv', 
+                            target_transform=vocab, memory=True)
 
+test_set = LJSpeechDataset(root=root, csv_file='test-subset.csv', 
+                           target_transform=vocab, memory=False)
 
+wave1, text1 = train_set[1] # [129693] , [32413]
+wave2, text2 =train_set[2]  # [98]     , [26]
 
+# Dataloader
+# for add pad to waves & texts
+def collate_fn(batch):
+  x, y = zip(*batch)
+  x = torch.nn.utils.rnn.pad_sequence(x, batch_first=True, padding_value=0).unsqueeze(1)
+  y = torch.nn.utils.rnn.pad_sequence(y, batch_first=True, padding_value=0)
+  return x, y
 
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 
+valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
 
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+waveforms, targets = next(iter(train_loader))
+waveforms.shape, targets.shape # [64, 1, 220061] , [64, 176]
+targets[0]
 
 
 
